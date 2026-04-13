5472410a00f8baf0b6951dde5eb3b1928a7dc7f9
Dima <wertdimapopo@gmail.com>
2026-04-13 18:37:36 +0200

    Updated design and added donation page

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/* Плавная прокрутка при переходе по ссылкам */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+/// Анимация кнопок при наведении
+document.querySelectorAll('.btn-primary, .btn-secondary, .btn-back, .btn-donate').forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.1)';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
     });
 });
